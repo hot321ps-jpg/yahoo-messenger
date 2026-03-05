143d8e3352db5e32baca9f5c11069d5271603094
hot321ps-jpg <hot321ps@gmail.com>
2026-03-05 17:51:39 +0800

    Update ContactList.jsx

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -15,13 +15,23 @@ export default function ContactList({ onOpenChat }) {
       <div className="menu-bar">
         <span>即時通(Y)</span><span>聯絡人(C)</span>
       </div>
+      
+      {/* 更新了這裡的個人資訊區塊 */}
       <div className="my-profile">
         <div className="my-avatar">😎</div>
         <div>
           <div style={{fontWeight: 'bold'}}>我的帳號</div>
-          <select style={{marginTop: '5px'}}><option>✅ 上線</option></select>
+          <select style={{marginTop: '3px', fontSize: '11px', padding: '1px'}}>
+            <option>✅ 上線</option>
+            <option>👻 隱身</option>
+          </select>
+          {/* 加入正在收聽彩蛋 */}
+          <div className="music-status" title="點擊播放/暫停">
+            🎵 正在收聽：妥協 - 蔡依林
+          </div>
         </div>
       </div>
+
       <div className="buddy-list">
         <div className="group-header">▼ 我的好友群</div>
         {friends.map(friend => (
